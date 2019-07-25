c2a5ae981eec923049fed436b69614cfa9b4b8a2
Pekka Turtola <pekka.turtola@yap.fi>
2019-07-26 01:14:08 +0300

    Complete 4.5

--- tests/utils/list_helper.test.js
+++ tests/utils/list_helper.test.js
@@ -1,4 +1,27 @@
-const { dummy, totalLikes } = require('../../utils/list_helper');
+const { dummy, totalLikes, favoriteBlog } = require('../../utils/list_helper');
+
+const fakeBlogs = [{
+  title: 'foo1',
+  author: 'bar1',
+  url: 'https://foo1.bar',
+  likes: 3
+}, {
+  title: 'foo2',
+  author: 'bar2',
+  url: 'https://foo2.bar',
+  likes: 4
+
+}, {
+  title: 'foo3',
+  author: 'bar3',
+  url: 'https://foo3.bar',
+  likes: 7
+}, {
+  title: 'foo4',
+  author: 'bar4',
+  url: 'https://foo4.bar',
+  likes: 0
+}];
 
 describe('dummy util', () => {
   test('should return 1', () => {
@@ -18,24 +41,22 @@ describe('total likes', () => {
 
   test('should return sum of likes of all blogs', () => {
     const expected = 14;
-    const blogs = [{
-        title: 'foo1',
-        author: 'bar1',
-        url: 'https://foo1.bar',
-        likes: 3
-      }, {
-        title: 'foo2',
-        author: 'bar2',
-        url: 'https://foo2.bar',
-        likes: 4
+    expect(totalLikes(fakeBlogs)).toBe(expected);
+  });
+});
 
-      }, {
-        title: 'foo3',
-        author: 'bar3',
-        url: 'https://foo3.bar',
-        likes: 7
-      }
-    ]
-    expect(totalLikes(blogs)).toBe(expected);
+describe('favorite blog', () => {
+  test('should return undefined when no blogs exist', () => {
+    expect(favoriteBlog([])).toBe(undefined);
+  });
+
+  test('should return a single blog', () => {
+    const onlyBlog = { title: 'bar', author: 'foo', likes: 0 };
+    expect(favoriteBlog([onlyBlog])).toEqual(onlyBlog);
+  });
+
+  test('should return greatest blog of all blogs', () => {
+    const expectedTitle = 'foo3';
+    expect(favoriteBlog(fakeBlogs).title).toBe(expectedTitle);
   });
 });
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,4 +2,12 @@ const dummy = foo => 1;
 
 const totalLikes = blogs => blogs && blogs.length && blogs.reduce((result, item) => result + item.likes, 0);
 
-module.exports = { dummy, totalLikes };
+const favoriteBlog = blogs => {
+  if (!blogs || !blogs.length) {
+    return undefined;
+  }
+  // Do not alter given array - slice it
+  return blogs.slice().sort((a, b) => a.likes > b.likes ? -1 : 1)[0];
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog };
