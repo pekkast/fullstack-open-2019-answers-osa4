f82cd4987d1dc93dd5f53d2375350492f556711f
Pekka Turtola <pekka.turtola@yap.fi>
2019-07-25 21:32:15 +0300

    Complete 4.2

--- index.js
+++ index.js
@@ -4,38 +4,13 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const config = require('./utils/config');
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
+const blogsController = require('./controllers/blogs');
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true });
 
 app.use(cors());
 app.use(bodyParser.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs);
-    });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result);
-    });
-});
+app.use('/api/blogs', blogsController);
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
